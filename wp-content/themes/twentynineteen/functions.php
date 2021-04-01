7ed5297420dbca014edad084f3145ec0e17aebf9
rodriguetzmauricio <rodriguetzmauricio@gmail.com>
2021-04-01 17:56:18 -0300

    Creacion custom-post-type productos

--- wp-content/themes/twentynineteen/functions.php
+++ wp-content/themes/twentynineteen/functions.php
@@ -337,3 +337,30 @@ require get_template_directory() . '/inc/customizer.php';
  * Block Patterns.
  */
 require get_template_directory() . '/inc/block-patterns.php';
+
+
+
+function productos_type(){
+	$labels =  array(
+		'name' => 'Productos',
+		'singular_name' => 'Producto',
+		'menu_name' => 'Productos',
+	);
+	$args = array(
+		'label' => 'Productos',
+		'description' => 'Productos',
+		'labels' => $labels,
+		'supports' => array('title','editor','thumbnail','revisions'),
+		'public' => true,
+		'show_in_menu' => true,
+		'menu_position' =>5,
+		'menu_icon' => 'dashicons-cart',
+		'can_export' => true,
+		'publicly_queryable' => true,
+		'rewrite' => true,
+		'show_in_rest' => true,
+	);
+	register_post_type('producto', $args);
+}
+
+add_action('init','productos_type');
